95578706fb2ba63c324140085b3a13a170473576
yahya shawky <yahyashaeky@gmail.com>
2026-06-14 13:33:53 +0300

    fix: rename file to match case-sensitive import

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
 import PageNav from "../components/PageNav";
 
-export default function Homepage() {
+export default function HomePage() {
   return (
     <main className={styles.homepage}>
         <PageNav />
